230af53e905ed46e713d0e50120f0ca964e16030
Momen Ashraf <momenelmesady4803@gmail.com>
2024-02-12 18:02:01 +0200

    add security middlewares

--- app.js
+++ app.js
@@ -2,16 +2,36 @@ const express = require("express")
 const dotenv = require("dotenv")
 dotenv.config({ path: "./.env" })
 const path = require("path")
+const cookieParser = require('cookie-parser');
+// security 
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+// routers
 const addressRouter = require("./routes/addressRouter")
 const userRouter = require("./routes/userRouter")
-const cookieParser = require('cookie-parser');
 const authRouter = require("./routes/authRouter")
 const categoryRouter = require("./routes/categoryRouter")
 const productRouter = require("./routes/productRouter")
 const cartRouter = require("./routes/cartRouter")
 const orderRouter = require("./routes/orderRouter")
 const reviewRouter = require("./routes/reviewRouter")
-const app = express()
+
+
+const app = express();
+
+// Apply rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use('/', limiter);
+app.use(helmet());
+app.use(hpp());
+app.use(xss());
 
 
 app.use(express.json());
