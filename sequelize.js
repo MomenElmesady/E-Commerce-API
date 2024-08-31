8acc9f70b7c366872fbe5f04b01dc24731031cb7
Momen Ashraf <momenelmesady4803@gmail.com>
2024-08-31 21:38:08 +0300

    add address and ci

--- sequelize.js
+++ sequelize.js
@@ -1,9 +1,15 @@
 // sequelize.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ecommerce', 'root', 'Momen@@010', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME, // Database name
+  process.env.DATABASE_USER, // Username
+  process.env.DATABASE_PASSWORD, // Password
+  {
+    host: process.env.DATABASE_HOST, // Hostname
+    dialect: 'mysql', // Database dialect
+    port: process.env.DATABASE_PORT || 3306, // Port (default 3306)
+  }
+);
 
 module.exports = sequelize;
