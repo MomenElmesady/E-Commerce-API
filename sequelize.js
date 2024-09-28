0f9c300108759d9f8736d08ececfa2afc3a17a1e
Momen Ashraf <momenelmesady4803@gmail.com>
2024-09-28 14:25:16 +0300

    prepare for deploy

--- sequelize.js
+++ sequelize.js
@@ -5,8 +5,4 @@ const sequelize = new Sequelize('sql12733949', 'sql12733949', 'fw7dzRt19k', {
   host: 'sql12.freesqldatabase.com',
   dialect: 'mysql',
 });
-<<<<<<< HEAD
-module.exports = sequelize;
-=======
 module.exports = sequelize;
->>>>>>> 07f665e2897beddd8261858bfd611b44ebd9ae60
