20dabc05257d0042df199fe7d6da2c09186ef8aa
3oraby <mohaahmed21102003@gmail.com>
2024-09-28 15:06:41 +0300

    deploy

--- sequelize.js
+++ sequelize.js
@@ -1,9 +1,8 @@
 // sequelize.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ecommerce', 'oraby', '01011088624@tito', {
-  host: 'localhost',
+const sequelize = new Sequelize('sql12733949', 'sql12733949', 'fw7dzRt19k', {
+  host: 'sql12.freesqldatabase.com',
   dialect: 'mysql',
 });
-
-module.exports = sequelize;
+module.exports = sequelize;
